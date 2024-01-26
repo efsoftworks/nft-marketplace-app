1bdc09b1dd28860d73b60d63f11741f6c559b8da
= <info@farunurisonmez.com>
2024-01-26 13:27:46 +0300

    Store Smart Contract Addresses #41
    Returns relevant smart contract addresses.
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ src/app/contants/contractAddresses.ts
@@ -0,0 +1,8 @@
+
+/**********************************************************
+ * Token Contract Deployed - Ethereum Addresses
+ * Documentation: https://ethereum.org/en/developers/docs/accounts/
+ * Documentation: https://ethereum.org/en/developers/docs/smart-contracts/deploying/
+ **********************************************************/
+
+export const nftContract = "";
