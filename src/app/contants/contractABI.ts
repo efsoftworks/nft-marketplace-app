01b3cc57803bd1f73ba43bf417995a61054cbc21
= <info@farunurisonmez.com>
2024-01-26 13:33:10 +0300

    Store Smart Contract ABI #42
    Returns the ABI of the relevant smart contract.
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ src/app/contants/contractABI.ts
@@ -0,0 +1,10 @@
+/**********************************************************
+ * ABI (Application Binary Interface): Contract's Variables and Methods Information
+ * Documentation: https://docs.soliditylang.org/en/v0.5.3/abi-spec.html
+ **********************************************************/
+
+export const nftABI = [
+    {
+        
+    }
+]
