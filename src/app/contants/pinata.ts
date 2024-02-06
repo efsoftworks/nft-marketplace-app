93e8da3910437626c6dfa154c1719082e1bd7618
= <info@farunurisonmez.com>
2024-02-06 16:56:23 +0300

    Update environment variables in CI workflow #8
    - Define environment variables schema using Zod
    - Retrieve environment variables in a separate file
    - Update Infura API configuration to use environment variables from the defined schema
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- src/app/contants/pinata.ts
+++ src/app/contants/pinata.ts
@@ -2,11 +2,11 @@
  * Pinata : Pinata provides secure and verifiable files for your NFTs.
  * Docs: https://pinata.cloud/documentation#PinningServicesAPI
  **********************************************************/
-
+import { env } from "../../../env";
 const pinata = {
-    apiKey : process.env.NEXT_PUBLIC_PINATA_API_KEY,
-    secretApiKey : process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
-    jwt: process.env.NEXT_PUBLIC_PINATA_JWT,
+    apiKey : env.NEXT_PUBLIC_PINATA_API_KEY,
+    secretApiKey : env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
+    jwt: env.NEXT_PUBLIC_PINATA_JWT,
 };
 
 export default pinata;
